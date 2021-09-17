2328a71861b0943cdda7d9fbe2fa66430ce74961
MarcoDeRosso <mcoder@hotmail.it>
2021-09-17 15:57:51 +0200

    modale

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -24,11 +24,30 @@
                   <td>{{$post->date}}</td>
                   <td><a href="{{route('posts.show',['post'=>$post->id])}}"><button type="button" class="btn btn-primary"><i class="bi bi-zoom-in"></i></button></a>
                     <a href="{{route('posts.edit', $post)}}"><button type="button" class="btn btn-warning"><i class="bi bi-pencil"></i></button></a>
-                    <form action="{{route('posts.destroy', $post)}}" method="POST">
-                      @csrf
-                      @method('DELETE')
-                      <button type="submit" class="btn btn-danger"><i class="bi bi-trash"></i></button>
-                    </form>
+                    <button type="submit" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal"><i class="bi bi-trash"></i></button>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                      <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Attenzione!</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body">
+                            Sei sicuro di voler eliminare l'elemento?
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Torna indietro</button>
+                            <form action="{{route('posts.destroy', $post)}}" method="POST">
+                              @csrf
+                              @method('DELETE')
+                              <button type="submit" class="btn btn-danger">Elimina</button>
+                            </form>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                   </td>
               </tr>
           @endforeach
